927b2b46b5a35f8d84a83c51ad56665df042465b
AngryPapayah <airissavermo@hotmail.com>
2026-06-28 14:49:20 +0200

    languages

--- src/context/SettingsContext.js
+++ src/context/SettingsContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import translations from '../i18n/translations';
+import translations from '../languages/translations';
 
 const COLORS = {
   dark: {
